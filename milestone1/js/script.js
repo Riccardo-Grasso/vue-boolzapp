8ca2b966cba8b8cbfdda1619ae78e737aff4f3fe
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-03 22:11:39 +0100

    js data

--- milestone1/js/script.js
+++ milestone1/js/script.js
@@ -1,1 +1,25 @@
 Vue.config.devtools = true;
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        chat: [
+            {
+                nome: "Michele",
+                img: "img/avatar_1.jpg"
+            },
+            {
+                nome: "Giulia",
+                img: "img/avatar_io.jpg"
+            },
+            {
+                nome: "Sara",
+                img: "img/avatar_6.jpg"
+            },
+            {
+                nome: "Daniele",
+                img: "img/avatar_5.jpg"
+            }
+        ]
+    }
+});
